7272c5a1fa30162efafdfb35286b60af2fd390a9
Lizza Reyes <lizzareyes13@gmail.com>
2016-10-25 17:49:42 -0400

    added code to the script.js file

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+// Check if the user is ready to play!
+
+confirm("I'm ready to play!");
+
+var age = prompt("What's your age?");
+
+if (age < 13){
+    console.log("Ok. you can play but I take no responsibility.")
+    }
+else{
+    console.log("great! you're ready to play")
+    }
+
+    console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'")
+
+var userAnswer = prompt("Do you want to race Bieber on stage?");
+// var yes = yes;
+// var no = no;
+if (userAnswer === "yes"){
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!")
+    }
+else{
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'")
+    }
+
+    var feedback = prompt("can you rate my game from the scale of 1 thru 10?");
+
+    if (feedback > 8){
+        console.log("Thank you! We should race at the next concert!");
+        }
+        else{
+            console.log("I'll keep practicing coding and racing.");
+            }
